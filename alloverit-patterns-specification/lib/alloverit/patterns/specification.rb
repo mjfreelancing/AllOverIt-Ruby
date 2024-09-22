dc3e877366bf290e892f0d062c6f845864c3c5df
Malcolm Smith <malcolm@mjfreelancing.com>
2024-09-22 14:07:31 +1000

    Comment updates

--- alloverit-patterns-specification/lib/alloverit/patterns/specification.rb
+++ alloverit-patterns-specification/lib/alloverit/patterns/specification.rb
@@ -8,10 +8,10 @@ require_relative "specification/not_specification"
 
 module Alloverit
   module Patterns
-    # The `Specification` module defines a set of methods for creating and combining specifications. Each method
+    # The Specification module defines a set of methods for creating and combining specifications. Each method
     # returns a new specification object, allowing for composition and chaining of different specifications.
     #
-    # Implementations of this module must define the `satisfied_by?` method to specify the actual criteria.
+    # Implementations of this module must define the *satisfied_by?* method to specify the actual criteria.
     module Specification
       # This method must be implemented by any class including this module. It should return a boolean indicating
       # whether the given `candidate` satisfies the specification.
@@ -20,34 +20,34 @@ module Alloverit
       end
 
       # Combines the current specification with another specification using a logical AND. Returns a new
-      # `AndSpecification` object that represents the combined criteria.
+      # AndSpecification object that represents the combined criteria.
       def and(other)
         instance = instantiate_if_class(other)
         AndSpecification.new(self, instance)
       end
 
       # Combines the current specification with another specification using a logical AND NOT. Returns a new
-      # `AndNotSpecification` object that represents the combined criteria.
+      # AndNotSpecification object that represents the combined criteria.
       def and_not(other)
         instance = instantiate_if_class(other)
         AndNotSpecification.new(self, instance)
       end
 
       # Combines the current specification with another specification using a logical OR. Returns a new
-      # `OrSpecification` object that represents the combined criteria.
+      # OrSpecification object that represents the combined criteria.
       def or(other)
         instance = instantiate_if_class(other)
         OrSpecification.new(self, instance)
       end
 
       # Combines the current specification with another specification using a logical OR NOT. Returns a new
-      # `OrNotSpecification` object that represents the combined criteria.
+      # OrNotSpecification object that represents the combined criteria.
       def or_not(other)
         instance = instantiate_if_class(other)
         OrNotSpecification.new(self, instance)
       end
 
-      # Negates the current specification. Returns a new `NotSpecification` object that represents the negated criteria.
+      # Negates the current specification. Returns a new NotSpecification object that represents the negated criteria.
       def not
         NotSpecification.new(self)
       end
--- alloverit-patterns-specification/lib/alloverit/patterns/specification/composite_specification.rb
+++ alloverit-patterns-specification/lib/alloverit/patterns/specification/composite_specification.rb
@@ -10,34 +10,34 @@ module Alloverit
         include Specification
 
         # Class-level method for combining two specifications using a logical AND. Returns a new
-        # `AndSpecification` object that represents the combined criteria.
+        # AndSpecification object that represents the combined criteria.
         def self.and(other)
           instance = instantiate_if_class(other)
           new.and(instance)
         end
 
         # Class-level method for combining two specifications using a logical AND NOT. Returns a new
-        # `AndNotSpecification` object that represents the combined criteria.
+        # AndNotSpecification object that represents the combined criteria.
         def self.and_not(other)
           instance = instantiate_if_class(other)
           new.and_not(instance)
         end
 
         # Class-level method for combining two specifications using a logical OR. Returns a new
-        # `OrSpecification` object that represents the combined criteria.
+        # OrSpecification object that represents the combined criteria.
         def self.or(other)
           instance = instantiate_if_class(other)
           new.or(instance)
         end
 
         # Class-level method for combining two specifications using a logical OR NOT. Returns a new
-        # `OrNotSpecification` object that represents the combined criteria.
+        # OrNotSpecification object that represents the combined criteria.
         def self.or_not(other)
           instance = instantiate_if_class(other)
           new.or_not(instance)
         end
 
-        # Class-level method for negating a specification. Returns a new `NotSpecification` object that represents
+        # Class-level method for negating a specification. Returns a new NotSpecification object that represents
         # the negated criteria.
         def self.not
           new.not
