9982f6ceb20d897f13097ff904d0297b1ad5f4df
Malcolm Smith <malcolm@mjfreelancing.com>
2024-09-23 19:46:08 +1000

    Allow pipeline step to accept an object with call(input)

--- alloverit-patterns-pipeline/lib/alloverit/patterns/pipeline.rb
+++ alloverit-patterns-pipeline/lib/alloverit/patterns/pipeline.rb
@@ -26,6 +26,10 @@ module AllOverIt
           @steps = []
         end
 
+        # pipeline_step can be an instance of PipelineStep or any other class that responds to :call(input) and
+        # returns the output to be provided to the next step.
+        #
+        # block, if provided, must accept an 'input' and return the output to be provided to the next step.
         def step(pipeline_step = nil, &block)
           if block_given?
             raise ArgumentError, "Cannot provide both a pipeline step and a block" if pipeline_step
@@ -33,8 +37,8 @@ module AllOverIt
             pipeline_step = BlockStep.new(&block)
           end
 
-          unless pipeline_step.is_a?(PipelineStep)
-            raise ArgumentError, "A pipeline step must inherit #{PipelineStep.name}"
+          unless pipeline_step.is_a?(PipelineStep) || (pipeline_step.respond_to?(:call) && pipeline_step.method(:call).arity == 1)
+            raise ArgumentError, "A pipeline step must inherit #{PipelineStep.name} or have a call method that accepts exactly one argument"
           end
 
           @steps << pipeline_step
