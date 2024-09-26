5a5179d0d6fee4344c6a579ad0b3a6b244881f34
Malcolm Smith <malcolm@mjfreelancing.com>
2024-09-26 23:07:44 +1000

    doc and rbs updates

--- alloverit-utils/lib/alloverit/utils.rb
+++ alloverit-utils/lib/alloverit/utils.rb
@@ -2,32 +2,33 @@
 
 module AllOverIt
   module Utils
-    # @param instance_or_class An instance of an object or a class type.
-    # @return The original instance if not a Class type, otherwise a newly constructed instance of the Class type.
+    # Converts a class or an instance to an instance.
+    #
+    # This method checks whether the argument is a class or an instance.
+    # If a class is provided, it instantiates the class. If an instance is provided,
+    # it returns the instance as is.
+    #
+    # @param instance_or_class [Class, Object] A class or an instance.
+    # @return [Object] An instance of the provided class or the original object
+    #   if it was already an instance.
     def self.as_instance(instance_or_class)
       instance_or_class.is_a?(Class) ? instance_or_class.new : instance_or_class
     end
 
-    # @param The object to check if its' class ancestors include the specified class_type.
-    # @param class_type The class type to check exists in the ancestor chain of the provided instance.
-    # @return The original instance if its ancestor chain includes the specified class_type.
-    # @raise ArgumentError When the instance's ancestor chain does not include the specified class_type.
+    # @param instance [Object] The object to check if its' class ancestor chain includes the specified class_type.
+    # @param class_type [Class] The class type to check exists in the ancestor chain of the provided instance.
+    # @return The original instance if its ancestor chain includes the specified class_type, otherwise an ArgumentError is raised.
     def self.ensure_instance_includes(instance, class_type)
-      unless instance.class.ancestors.include?(class_type)
-        raise ArgumentError, "Expected #{instance.class} to include #{class_type.name}"
-      end
+      raise ArgumentError, "Expected #{instance.class} to include #{class_type.name}" unless instance.class.ancestors.include?(class_type)
 
       instance
     end
 
-    # @param The object to check if it inherits the specified class_type.
-    # @param class_type The class type is a super class of the provided instance.
-    # @return The original instance if it inherits the specified class_type.
-    # @raise ArgumentError When the instance does not inherit the specified class_type.
+    # @param instance [Object] The object to check if it inherits the specified class_type.
+    # @param class_type [Class] The class type to check is a super class of the provided instance.
+    # @return The original instance if it inherits the specified class_type, otherwise an ArgumentError is raised.
     def self.ensure_instance_is_a(instance, class_type)
-      unless instance.is_a?(class_type)
-        raise ArgumentError, "Expected #{instance.class} to inherit #{class_type.name}"
-      end
+      raise ArgumentError, "Expected #{instance.class} to inherit #{class_type.name}" unless instance.is_a?(class_type)
 
       instance
     end
