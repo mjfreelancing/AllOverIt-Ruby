48feb78c113080f856541f549b6043499a7de93a
Malcolm Smith <malcolm@mjfreelancing.com>
2024-09-27 23:46:29 +1000

    utils update

--- alloverit-utils/lib/alloverit/utils.rb
+++ alloverit-utils/lib/alloverit/utils.rb
@@ -17,16 +17,18 @@ module AllOverIt
 
     # @param instance [Object] The object to check if its' class ancestor chain includes the specified class_type.
     # @param class_type [Class] The class type to check exists in the ancestor chain of the provided instance.
-    # @return The original instance if its ancestor chain includes the specified class_type, otherwise an ArgumentError is raised.
+    # @return The original instance if its ancestor chain includes the specified class_type,
+    #   otherwise an ArgumentError is raised.
     def self.ensure_instance_includes(instance, class_type)
       raise ArgumentError, "Expected #{instance.class} to include #{class_type.name}" unless instance.class.ancestors.include?(class_type)
 
       instance
     end
 
-    # @param instance [Object] The object to check if it inherits the specified class_type.
+    # @param instance [Object] The object to check if it is of, or inherits, the specified class_type.
     # @param class_type [Class] The class type to check is a super class of the provided instance.
-    # @return The original instance if it inherits the specified class_type, otherwise an ArgumentError is raised.
+    # @return The original instance if it is of, or inherits, the specified class_type,
+    #   otherwise an ArgumentError is raised.
     def self.ensure_instance_is_a(instance, class_type)
       raise ArgumentError, "Expected #{instance.class} to inherit #{class_type.name}" unless instance.is_a?(class_type)
 
