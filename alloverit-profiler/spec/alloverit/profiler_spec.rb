e87af21a043b799457f01255cb0e75cd39036d44
Malcolm Smith <malcolm@mjfreelancing.com>
2024-10-20 15:27:50 +1100

    Add initial profiler gem code

---
+++ alloverit-profiler/spec/alloverit/profiler_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Alloverit::Profiler do
+  it "has a version number" do
+    expect(Alloverit::Profiler::VERSION).not_to be nil
+  end
+
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
+end
